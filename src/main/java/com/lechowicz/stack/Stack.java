1c1f02353b3cf1e28598dc146a9cb81cdab477b2
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-07-28 13:38:26 +0200

    Add skeleton of stack

--- src/main/java/com/lechowicz/stack/Stack.java
+++ src/main/java/com/lechowicz/stack/Stack.java
@@ -1,4 +1,21 @@
 package com.lechowicz.stack;
 
-public class Stack {
+public class Stack<E> {
+
+    public Stack(){
+
+    }
+
+    public void push(E item){
+
+    }
+
+    public E pop(){
+        return null;
+    }
+
+    public E peek(){
+        return null;
+    }
+
 }
